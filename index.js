b0d745f6c1ac66e24b2cd0f5571d0d48ddccfec1
ili2908 <38957777+ili2908@users.noreply.github.com>
2024-09-30 17:02:54 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+export default {
+    pluginName: "Tets Plugin",
+    pluginVersion: "0.0.1",
+    rightPanels: [],
+    leftPanels: [],
+    rightClickPanels: [],
+    init: ()=>{console.log("I am alive")}
+    
+};
